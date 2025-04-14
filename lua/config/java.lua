84b6a68d75e17a14f78e868f2e3f037590706def
David Ruiz Diaz <adavidruizdiaz@gmail.com>
2025-04-14 07:26:44 -0300

    corrección de errores

--- lua/config/java.lua
+++ lua/config/java.lua
@@ -1,2 +1,16 @@
 require("java").setup()
-require("lspconfig").jdtls.setup({})
+require("lspconfig").jdtls.setup({
+  settings = {
+    java = {
+      configuration = {
+        runtimes = {
+          {
+            name = "openjdk-21",
+            path = "X:/apps_instaladas/Java_JDK_versions/openjdk-21+35/jdk-21",
+            default = true,
+          },
+        },
+      },
+    },
+  },
+})
